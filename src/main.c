8a3fd25a99e5075a1dd50d7d2224f600572bd0a0
engineAL <engineal@outlook.com>
2014-11-11 11:38:25 -0500

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Aaron Lucia
+ *
+ * Created on November 11, 2014, 11:11 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
