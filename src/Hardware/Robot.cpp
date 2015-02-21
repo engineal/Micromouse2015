e76d0dd461c711317f5d32c20d51f51251f21a59
engineAL <engineal@outlook.com>
2015-02-21 11:19:25 -0500

    First hardware control

--- src/Hardware/Robot.cpp
+++ src/Hardware/Robot.cpp
@@ -4,8 +4,18 @@
  *
  * Created on February 7, 2015, 10:51 AM
  */
+#include <avr/io.h>
+
 namespace Hardware {
 
     class Robot {
+        void initilize() {
+            //Set digital pins 0 and 1 to be output
+            DDRD |= (1 << PORTD1) | (1 << PORTD2);
+        }
+        
+        void setPort() {
+            PORTD |= 0b10101000;
+        }
     };
 }
