936fceb1b02acd1a1a89e428f43f32c7422c4a53
catalyst995 <dpare@umass.edu>
2015-02-21 11:10:51 -0500

    Folder for code collections.

--- src/Hardware/Robot.cpp
+++ src/Hardware/Robot.cpp
@@ -1,11 +1,22 @@
-/* 
+/***************
  * File:   Robot.cpp
  * Author: Aaron Lucia
  *
  * Created on February 7, 2015, 10:51 AM
- */
-namespace Hardware {
+ * Defines and controls hardware elements of the robot
+ **************/
 
+#include <avr/io.h>
+
+
+namespace Hardware {
+    
+    enum Sensor {rightb, rightf, leftb, leftf, frontl, frontr, backl, backr, encoder, encodel};
+    
     class Robot {
+        
+        int readSensor (Sensor sensor) {
+            
+        }
     };
 }
