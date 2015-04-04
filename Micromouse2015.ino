fa637bc28f29743c22717468a10e13fb8404fa87
engineAL <engineal@outlook.com>
2015-04-04 18:05:51 -0400

    Test code

--- Micromouse2015.ino
+++ Micromouse2015.ino
@@ -8,23 +8,28 @@
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
 
+int mode;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Serial connected");
+  
+  mode = 0;
+  digitalWrite(A5, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int mode = (digitalRead(9) ? B1 : 0) | (digitalRead(10) ? B10 : 0) | (digitalRead(11) ? B100 : 0) | (digitalRead(12) ? B1000 : 0);
+  //int mode = (digitalRead(9) ? B1 : 0) | (digitalRead(10) ? B10 : 0) | (digitalRead(11) ? B100 : 0) | (digitalRead(12) ? B1000 : 0);
   Serial.println(mode);
   digitalWrite(4, bitRead(mode, 0));
   digitalWrite(7, bitRead(mode, 1));
   digitalWrite(8, bitRead(mode, 2));
+  mode++;
+  if (mode == 8) {
+    mode = 0;
+  }
 
-  digitalWrite(A5, HIGH);
-  delay(500);
-  digitalWrite(A5, LOW);
   delay(500);
 
   //Maze* maze = new Maze();
